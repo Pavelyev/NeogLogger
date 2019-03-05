731072d6e0a5ab212e001bc25a17d653d37b273f
Anton Pavelyev <pap_90@mail.ru>
2019-03-05 17:32:38 +0300

    initial

---
+++ NeonLogger/NeonLogger.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace NeonLogger
+{
+    public class NeonLogger
+    {
+        public void Log(string message)
+        {
+            if (_dict.ContainsKey(message))
+            {
+                _dict[message]++;
+            }
+            else
+            {
+                _dict[message] = 1;
+            }
+
+            lock (_lock)
+            {
+                File.AppendAllText(_filePath, message);
+            }
+        }
+
+        public string[] PopularMessages()
+        {
+            return _dict.OrderByDescending(x => x.Value)
+                .Take(10)
+                .Select(x => x.Key)
+                .ToArray();
+        }
+
+        public void LogDeffered(string message)
+
+        {
+            if (_queue.Count > 10000)
+            {
+                return;
+            }
+
+            _queue.Enqueue(message);
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                while (_queue.TryDequeue(out var message))
+                {
+                    Log(message);
+                }
+
+                Thread.SpinWait(1);
+            }
+        }
+
+        private readonly ConcurrentDictionary<string, int> _dict = new ConcurrentDictionary<string, int>();
+        private readonly ConcurrentQueue<string> _queue = new ConcurrentQueue<string>();
+        private readonly Object _lock = new Object();
+        private readonly string _filePath;
+
+        public NeonLogger(string filePath)
+        {
+            _filePath = filePath;
+        }
+    }
+}
