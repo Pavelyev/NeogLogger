bb9ad9d1bf44b21de8e8b83f6a317c5fb1e4c9c2
Anton Pavelyev <pap_90@mail.ru>
2019-03-10 13:38:10 +0300

    Tests

---
+++ NeonLogger.Tests/Test_NeonLogger.cs
@@ -0,0 +1,145 @@
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace NeonLogger.Tests
+{
+    [TestFixture]
+    public class Test_NeonLogger
+    {
+        [Test]
+        public void Test_Log_WriteToFile()
+        {
+            if (File.Exists("log.txt"))
+            {
+                File.Delete("log.txt");
+            }
+
+            var logger = new NeonLogger("log.txt");
+
+            logger.Log("apple");
+            logger.Log("banana");
+
+            var lines = File.ReadAllLines("log.txt").ToArray();
+            Assert.IsTrue(lines[0] == "apple");
+            Assert.IsTrue(lines[1] == "banana");
+        }
+
+        [Test]
+        public void Test_LogDeferred_NotWriteUntilFlush()
+        {
+            if (File.Exists("log.txt"))
+            {
+                File.Delete("log.txt");
+            }
+
+            var logger = new NeonLogger("log.txt");
+
+            logger.LogDeferred("apple");
+            logger.LogDeferred("banana");
+
+            Assert.IsFalse(File.Exists("log.txt"));
+        }
+
+        [Test]
+        public void Test_LogDeferred_WriteToFile()
+        {
+            if (File.Exists("log.txt"))
+            {
+                File.Delete("log.txt");
+            }
+
+            var logger = new NeonLogger("log.txt");
+
+            logger.LogDeferred("apple");
+            logger.LogDeferred("banana");
+            logger.Flush();
+
+            var lines = File.ReadAllLines("log.txt").ToArray();
+            Assert.IsTrue(lines[0] == "apple");
+            Assert.IsTrue(lines[1] == "banana");
+        }
+
+        [Test]
+        public void Test_LogAndLogDeferred_WriteToFile()
+        {
+            if (File.Exists("log.txt"))
+            {
+                File.Delete("log.txt");
+            }
+
+            var logger = new NeonLogger("log.txt");
+
+            logger.LogDeferred("apple");
+            logger.LogDeferred("banana");
+            logger.Log("case");
+            logger.Log("deer");
+            logger.Flush();
+
+            var lines = File.ReadAllLines("log.txt").ToArray();
+            Assert.IsTrue(lines[0] == "case");
+            Assert.IsTrue(lines[1] == "deer");
+            Assert.IsTrue(lines[2] == "apple");
+            Assert.IsTrue(lines[3] == "banana");
+        }
+
+        [Test]
+        public void Test_PopularMessages_DifferentTimesLogged()
+        {
+            var logger = new NeonLogger("log.txt");
+
+            logger.Log("apple");
+
+            logger.Log("banana");
+            logger.Log("banana");
+
+            var popular = logger.PopularMessages();
+            Assert.IsTrue(popular.Contains("apple"));
+            Assert.IsTrue(popular.Contains("banana"));
+            Assert.IsFalse(popular.Contains("pek"));
+        }
+
+        [Test]
+        public void Test_PopularMessages_TenLoggedTwiceAndTwoLoggedOnce()
+        {
+            var logger = new NeonLogger("log.txt");
+
+            logger.Log("apple");
+            logger.Log("apple");
+            logger.Log("banana");
+            logger.Log("banana");
+            logger.Log("case");
+            logger.Log("case");
+            logger.Log("deer");
+            logger.Log("deer");
+            logger.Log("eagle");
+            logger.Log("eagle");
+            logger.Log("fever");
+            logger.Log("fever");
+            logger.Log("gem");
+            logger.Log("gem");
+            logger.Log("hat");
+            logger.Log("hat");
+            logger.Log("idol");
+            logger.Log("idol");
+            logger.Log("jeep");
+            logger.Log("jeep");
+            logger.Log("kernel");
+            logger.Log("lance");
+
+            var popular = logger.PopularMessages();
+            Assert.IsTrue(popular.Contains("apple"));
+            Assert.IsTrue(popular.Contains("banana"));
+            Assert.IsTrue(popular.Contains("case"));
+            Assert.IsTrue(popular.Contains("deer"));
+            Assert.IsTrue(popular.Contains("eagle"));
+            Assert.IsTrue(popular.Contains("fever"));
+            Assert.IsTrue(popular.Contains("gem"));
+            Assert.IsTrue(popular.Contains("hat"));
+            Assert.IsTrue(popular.Contains("idol"));
+            Assert.IsTrue(popular.Contains("jeep"));
+            Assert.IsFalse(popular.Contains("kernel"));
+            Assert.IsFalse(popular.Contains("lance"));
+        }
+    }
+}
